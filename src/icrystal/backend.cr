f4c9a7d5dc4e83c1eadbdabc2d42488b2fbd6590
Brian J. Cardiff <bcardiff@gmail.com>
2023-09-20 22:23:25 -0300

    Use crystal-repl-server client for backend

--- src/icrystal/backend.cr
+++ src/icrystal/backend.cr
@@ -1,45 +1,30 @@
 require "http/client"
-require "crystal-repl-server/common/api"
+require "crystal-repl-server/client"
 
 include Crystal::Repl::Server::API
 
 module ICrystal
   class CrystalInterpreterBackend
-    @socket_path : String
-    @input : IO::Memory
-    @output : IO::Memory
-    @error : IO::Memory
-    @server : Process
-    @client : HTTP::Client
+    @client : Crystal::Repl::Server::Client
 
     def initialize(*, prelude = nil)
       @socket_path = File.tempname("crystal", ".sock")
 
-      @input = IO::Memory.new
-      @output = IO::Memory.new
-      @error = IO::Memory.new
-
       exec_dir = File.dirname(Process.executable_path || raise "Unable to find executable path")
-      @server = Process.new(Path[exec_dir, "crystal-repl-server"].to_s, {@socket_path}, input: @input, output: @output, error: @error)
+      crystal_repl_server_bin = Path[exec_dir, "crystal-repl-server"].to_s
 
-      @client = retry do
-        HTTP::Client.new(UNIXSocket.new(@socket_path))
-      end
+      @client = Crystal::Repl::Server::Client.start_server_and_connect(server: crystal_repl_server_bin, socket: @socket_path)
 
       # TODO assert status ok
-      @client.post("/v1/start")
+      @client.start
     end
 
     def close
-      @server.close
       @client.close
-      # TODO delete socket
-      # @socket.delete
     end
 
     def check_syntax(code) : SyntaxCheckResult
-      # TODO check status ok
-      response = CheckSyntaxResponse.from_json(@client.post("/v1/check_syntax", body: code).body)
+      response = @client.check_syntax(code)
       case response
       when CheckSyntaxSuccess
         ICrystal::SyntaxCheckResult.new(:ok)
@@ -51,15 +36,10 @@ module ICrystal
     end
 
     def eval(code, store_history) : ExecutionResult | SyntaxCheckResult
-      # TODO check status ok
-      response = EvalResponse.from_json(@client.post("/v1/eval", body: code).body)
+      response = @client.eval(code)
       case response
       when EvalSuccess
-        eval_stdout = @output.rewind.gets_to_end.presence
-        eval_stderr = @error.rewind.gets_to_end.presence
-        @output.clear
-        @error.clear
-        res = ExecutionResult.new(true, response.value, eval_stdout, eval_stderr)
+        ExecutionResult.new(true, response.value, @client.read_stdout, @client.read_stderr)
       when EvalSyntaxError
         to_icrystal_syntax_check_result(response.message)
       when EvalError
@@ -82,17 +62,6 @@ module ICrystal
       else
         ICrystal::SyntaxCheckResult.new(:error, message)
       end
-    end
-
-    private def retry
-      last_ex = nil
-      3.times do
-        return yield
-      rescue ex
-        sleep 0.1
-        last_ex = ex
-      end
-      raise last_ex.not_nil!
     end
   end
 end
